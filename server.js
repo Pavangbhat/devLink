8da1bfdc413d5b54645fe83e6904cbc84a4d675c
pavan <pavan@localhost.localdomain>
2020-11-28 19:56:12 +0530

    prepared for deployment

--- server.js
+++ server.js
@@ -32,7 +32,15 @@ app.use("/api", require("./routes/auth"));
 app.get("/", (req, res) => {
   res.send("this is a get req");
 });
+// Server static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
 
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
